ffcf5e85efebabdfcca8b3ef730a738e9a29828a
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-16 09:46:01 +0000

    changed tests to test STDOUT, and implemented in method

--- fizzbuzz-extreme/lib/fizzbuzz.rb
+++ fizzbuzz-extreme/lib/fizzbuzz.rb
@@ -1,9 +1,12 @@
+public
+
 def fizzbuzz(num,f=3, b=5)
-	(0..num).to_a.map do |n| 
+	a = (0..num).to_a.map do |n| 
 		if n%f==0
-			n%b==0 ? 'Fizzbuzz' : 'Fizz'
+			n%b==0 ?  'Fizzbuzz' : 'Fizz'
 		else
 			n%b==0 ? 'Buzz' : n
 		end
 	end
+	puts a
 end
--- fizzbuzz-extreme/spec/fizzbuzz_spec.rb
+++ fizzbuzz-extreme/spec/fizzbuzz_spec.rb
@@ -1,19 +1,26 @@
 require './lib/fizzbuzz.rb'
 
 describe "FizzbuzzExtreme" do
+
 	it "should take an argument that determines it's range" do
-		expect(fizzbuzz(2)).to eq ['Fizzbuzz',1,2]
+		expect($stdout).to receive(:puts).with ["Fizzbuzz", 1, 2]
+		fizzbuzz(2)
 	end
 
 	it "should take a second argument, multiples of which are output as 'Fizz'" do
-		expect(fizzbuzz(3,3)).to eq ['Fizzbuzz',1,2,'Fizz']
+		expect($stdout).to receive(:puts).with ['Fizzbuzz',1,2,'Fizz']
+		fizzbuzz(3,3)
 	end
 
 	it "should take a third argument, multiples of which are output as 'Buzz'" do
-		expect(fizzbuzz(6,3,5)).to eq ['Fizzbuzz',1,2,'Fizz',4,'Buzz','Fizz']
+		expect($stdout).to receive(:puts).with ['Fizzbuzz',1,2,'Fizz',4,'Buzz','Fizz']
+		fizzbuzz(6,3,5)
 	end
 
 	it "should output multiples of Fizz and Buzz as 'Fizzbuzz" do
-		expect(fizzbuzz(15,3,5).select{|n| n=='Fizzbuzz'}.count).to eq 2
+		expect($stdout).to receive(:puts).with ['Fizzbuzz',1,2,'Fizz',
+																						4,'Buzz','Fizz',7,8,'Fizz',
+																						'Buzz',11,'Fizz',13,14,'Fizzbuzz']
+		fizzbuzz(15,3,5)
 	end
 end	
