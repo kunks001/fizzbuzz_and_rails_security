23d969d3d15251946e50cc2cc17b5269e7b975dd
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-16 08:43:19 +0000

    refactored method

--- fizzbuzz-extreme/lib/fizzbuzz.rb
+++ fizzbuzz-extreme/lib/fizzbuzz.rb
@@ -2,10 +2,8 @@ def fizzbuzz(num,f=3, b=5)
 	(0..num).to_a.map do |n| 
 		if n%f==0
 			n%b==0 ? 'Fizzbuzz' : 'Fizz'
-		elsif n%b==0
-			'Buzz'
-		else
-			n
+		else n%b==0
+			n%b==0 ? 'Buzz' : n
 		end
 	end
 end
