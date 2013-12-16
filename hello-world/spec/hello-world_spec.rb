a21b0fff7d334dad31b74a21c3abb1013ade5e66
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-12-16 09:56:59 +0000

    completed 'hello world'

--- hello-world/spec/hello-world_spec.rb
+++ hello-world/spec/hello-world_spec.rb
@@ -1,7 +1,8 @@
-require './spec/hello-world.rb'
+require './lib/hello-world.rb'
 
 describe "HelloWorld" do
-	it 'should output hello world and a newline character to the console'
+	it 'should output hello world and a newline character to the console' do
 		expect($stdout).to receive(:puts).with('Hello world')
+		hello_world
 	end
 end
